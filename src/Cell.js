2ccdda5aa3358b9bcea7554a3b0bce1e95e9d7ac
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-21 09:31:53 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -2,27 +2,22 @@ import React, { Component } from 'react';
 
 export default class Cell extends Component {
 
-  constructor(props) {
-    super()
-    this.state = {
-      color: props.value
-    }
+constructor(props) {
+  super(props)
+  this.state = {
+      color: this.props.value
   }
+}
 
-  handleClick = () => {
-    this.setState({
-      color: '#333'
-    })
+ clickEvent = () => {
+    const newColor = '#333'
+    this.setState({ color: newColor })
   }
-
-  render() {
+render() {
     return (
-      <div
-        className="cell"
-        style={{backgroundColor: this.state.color}}
-        onClick={this.handleClick}>
-      </div>
+    <div className="cell" style={{backgroundColor: this.state.color }} onClick={this.clickEvent}>
+    {this.state.color}
+    </div>
     )
-  }
-
+}
 }
